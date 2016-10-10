8ee9fe6fe671296045bc13d59f2b1d2653865377
IvetteCardona22 <IvetteCardona22@172.20.15.83>
2016-10-10 12:02:40 -0600

    Mi primer commit

---
+++ src/cualquiercosa.java
@@ -0,0 +1,23 @@
+
+public class cualquiercosa {
+	public cualquiercosa(int hola, int num) {
+		super();
+		this.hola = hola;
+		this.num = num;
+	}
+	private int hola;
+	private int num;
+	public int getHola() {
+		return hola;
+	}
+	public void setHola(int hola) {
+		this.hola = hola;
+	}
+	public int getNum() {
+		return num;
+	}
+	public void setNum(int num) {
+		this.num = num;
+	}
+	
+}
